48d0520135ec7f497fed0bf24c0b8a246463f97d
Russell Groover <russell.groover@gmail.com>
2025-04-20 13:49:16 -0400

    Updating About page with latest changes.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,7 +13,7 @@ function About() {
             }}
         >
             <Grid2 container>
-                <Grid2 size={{ xs: 12, md: 4 }}>
+                <Grid2 size={{ xs: 12, md: 2 }}>
                     <Stack direction='column' spacing={0}>
                         <Box sx={{
                             display: 'flex',
@@ -60,7 +60,7 @@ function About() {
                             <ListItem sx={{ display: 'list-item' }}>
                                 <Typography>Hello, my name is Russell Groover and I wrote this app as a POC
                                     to show potential employers my proficiencies with React, Dotnet, Azure, architecture,
-                                    DevOps, etc. The frontend and backend sourcecode for this solution is
+                                    DevOps, etc. The frontend and backend source code for this solution is
                                     available on my GitHub.
                                 </Typography>
                             </ListItem>
@@ -116,14 +116,16 @@ function About() {
                             </ListItem>
                             <ListItem sx={{ display: 'list-item' }}>
                                 <Typography>The IMDB website has become bloated and hard to navigate so I thought it would be
-                                    fun to write a lightweight IMBD clone that performed a similar (albeit less feature rich)
-                                    experience where you can easily find movies, tv shows, and actors
+                                    fun to write a lightweight IMBD clone where you can easily find movies, tv shows, actors, and most importantly,
+                                    to know where a TV Show or Movie are streaming. Many titles appear and disappear from streaming platforms at the
+                                    start/end of the month. My "Watch Guide" functionality gives you quick info on where a TV Show or Movie is
+                                    currently available.
                                 </Typography>
                             </ListItem>
                             <ListItem sx={{ display: 'list-item' }}>
                                 <Typography>The frontend React app is an Azure Static Website, the backend is a dotnet core Azure App Service.
-                                    The source code is hosted on Github in two separate repos and both the frontend and backend are automatically built and deployed
-                                    upon checkin / merge to the main branch by using Github actions
+                                    The source code is hosted on Github in two separate repos and both the frontend and backend are automatically
+                                    built and deployed upon checkin / merge to the main branch via Github actions
                                 </Typography>
                             </ListItem>
                             <ListItem sx={{ display: 'list-item' }}>
@@ -156,7 +158,7 @@ function About() {
                                 <Typography>I made a point of making the UI responsive so renders well on both desktop and mobile browsers</Typography>
                             </ListItem>
                             <ListItem sx={{ display: 'list-item' }}>
-                                <Typography>99% of what you see is json data that is returned from the TMDB API via brokering through a dotnet Web API.
+                                <Typography>Most of the information you see is json data that is returned from the TMDB API via brokering through a dotnet Web API.
                                     View Models are used to map the API response into what the React app consumes. 
                                     Strongly typed Typescript view models and axios API calls are generated using the openapi-generator cli.
                                     The site "Json to C#" is used to generate C# view models based on what the TMDB API returns</Typography>
@@ -204,17 +206,34 @@ function About() {
                                 </Button>
                             </ListItem>
                             <ListItem >
-                                <Typography variant='h5'>Limitations / Future State</Typography>
+                                <Typography variant='h5'>More Technical Details</Typography>
                             </ListItem>
                             <ListItem sx={{ display: 'list-item' }}>
-                                <Typography>The TMDB API supports paging but I just return the first page in most cases, paging might be implemented in the future</Typography>
+                                <Typography>The TMDB API supports paging for many of its endpoints. On the main search page I use infinite scrolling to
+                                    request the next page of results.</Typography>
                             </ListItem>
                             <ListItem sx={{ display: 'list-item' }}>
-                                <Typography>Required Attribution: "This product uses the TMDB API but is not
-                                    endorsed or certified by TMDB"</Typography>
+                                <Typography>I'm using Auth0 for authentication for Favorites. I only use the email and user info for basic POC
+                                    functionality for Favorites. If you want to exercise the saving of Favorites you'll need to setup an
+                                    Auth0 account as well as setup Azure Cosmos DB as mentioned below</Typography>
                             </ListItem>
                             <ListItem sx={{ display: 'list-item' }}>
-                                <Typography>Required Attribution: "Streaming information courtesy of JustWatch"</Typography>
+                                <Typography>Favorites information is stored in Azure Cosmos DB so it's tied to the authenticated user.
+                                You'll need to setup a Cosmos DB in Azure if you want to exercise Favorites functionality.</Typography>
+                            </ListItem>
+                            <ListItem sx={{ display: 'list-item' }}>
+                                <Typography>Lazy loading of images is used in any case where there could be a long list of media images. At one point I
+                                    was getting a 429 because of eager fetching of all Actor, Movie, etc images. Now, lazy loading preserves bandwidth
+                                    and only requests an image when it comes into view</Typography>
+                            </ListItem>
+                            <ListItem >
+                                <Typography variant='h5'>Attribution</Typography>
+                            </ListItem>
+                            <ListItem sx={{ display: 'list-item' }}>
+                                <Typography>This product uses the TMDB API but is not endorsed or certified by TMDB</Typography>
+                            </ListItem>
+                            <ListItem sx={{ display: 'list-item' }}>
+                                <Typography>Streaming information courtesy of JustWatch and TMDB</Typography>
                             </ListItem>
                         </List>
                     </Stack> 
